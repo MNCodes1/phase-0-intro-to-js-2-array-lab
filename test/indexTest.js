2d64065684b7838238e49aad419b62dd76c0e7ab
Maryanne Kimani <maranne.kim@gmail.com>
2025-09-24 17:08:32 +0300

    completed

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,3 +1,9 @@
+beforeEach(function () {
+  cats.length = 0;
+
+  cats.push("Milo", "Otis", "Garfield");
+});
+
 require ( './helpers.js' );
 
 describe('index.js', function () {
@@ -79,3 +85,45 @@ describe('index.js', function () {
     });
   });
 });
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+// 1. Destructively append a cat to the end
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+// 2. Destructively prepend a cat to the beginning
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+// 3. Destructively remove the last cat
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+// 4. Destructively remove the first cat
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// 5. Non-destructively append a cat (returns new array)
+function appendCat(name) {
+  return [...cats, name];
+}
+
+// 6. Non-destructively prepend a cat (returns new array)
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+// 7. Non-destructively remove the last cat (returns new array)
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+// 8. Non-destructively remove the first cat (returns new array)
+function removeFirstCat() {
+  return cats.slice(1);
+}
